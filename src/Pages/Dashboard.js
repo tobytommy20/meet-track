fae328c39704584098636dde0dd9e798b2e9a91b
tobytommy20 <tomiwaepebinu@gmail.com>
2026-03-24 23:24:44 +0100

    complete fix

--- src/Pages/Dashboard.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function dashboard() {
-    return (<h2>Dashboard coming soon...</h2>);
-}
-exports.default = dashboard;
